9cda898c3fe4930498f9de42a826188f05e1f6d0
Vericulname <sangcraftergaming@gmail.com>
2026-06-02 16:56:19 +0700

    xử lý exeption cho Authziation

--- src/main/java/com/sangng/restaurant/exception/GlobalExceptionHandler.java
+++ src/main/java/com/sangng/restaurant/exception/GlobalExceptionHandler.java
@@ -1,10 +1,14 @@
 package com.sangng.restaurant.exception;
 
-import com.sangng.restaurant.respone.ApiRespone;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authorization.AuthorizationDeniedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.sangng.restaurant.respone.ApiRespone;
+
+import io.jsonwebtoken.JwtException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -23,5 +27,17 @@ public class GlobalExceptionHandler {
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(new ApiRespone("An unexpected error occurred: " + ex.getMessage(), null));
+    }
+    @ExceptionHandler(AuthorizationDeniedException.class)
+    public ResponseEntity<ApiRespone> handleAuthorizationDeniedException(AuthorizationDeniedException ex) {
+        return ResponseEntity
+                .status(HttpStatus.FORBIDDEN)
+                .body(new ApiRespone(ex.getMessage(), null));
+    }
+    @ExceptionHandler(JwtException.class)
+    public ResponseEntity<ApiRespone> handleJwtException(JwtException ex) {
+        return ResponseEntity
+                .status(HttpStatus.UNAUTHORIZED)
+                .body(new ApiRespone(ex.getMessage(), null));
     }
 }
