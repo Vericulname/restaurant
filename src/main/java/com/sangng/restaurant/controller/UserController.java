5fd9a6d31eba3ca979b84063a53fd019a087544d
Vericulname <sangcraftergaming@gmail.com>
2026-06-04 14:16:03 +0700

    chỉ cho admin mới có quyền chỉnh sửa, xem danh sách user

--- src/main/java/com/sangng/restaurant/controller/UserController.java
+++ src/main/java/com/sangng/restaurant/controller/UserController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,6 +27,7 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequestMapping("${api.prefix}/users")
 @RequiredArgsConstructor
+@PreAuthorize("hasRole('ADMIN')") 
 public class UserController {
 
     private final IUserService userService;
