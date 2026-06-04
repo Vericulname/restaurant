401b107e698accbb12b7423271528b1dd58112a4
Vericulname <sangcraftergaming@gmail.com>
2026-06-04 14:12:38 +0700

    thêm chức năng đăng ký người dùng (role default là ROLE_USER)

--- src/main/java/com/sangng/restaurant/controller/AuthController.java
+++ src/main/java/com/sangng/restaurant/controller/AuthController.java
@@ -1,5 +1,7 @@
 package com.sangng.restaurant.controller;
 
+import java.lang.reflect.Array;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -12,11 +14,15 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.sangng.restaurant.dto.UserDto;
+import com.sangng.restaurant.model.User;
 import com.sangng.restaurant.request.LoginRequest;
+import com.sangng.restaurant.request.UserCreateRequest;
 import com.sangng.restaurant.respone.ApiRespone;
 import com.sangng.restaurant.respone.AuthRespone;
 import com.sangng.restaurant.security.JWT.JWTUtils;
 import com.sangng.restaurant.security.user.BillUserDetail;
+import com.sangng.restaurant.service.User.IUserService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -26,6 +32,7 @@ import lombok.RequiredArgsConstructor;
 public class AuthController {
 
     private final AuthenticationManager authManager;
+    private final IUserService userService;
     private final JWTUtils jwtUtils;
 
     @PostMapping("/login")
@@ -40,6 +47,27 @@ public class AuthController {
             AuthRespone authRespone = new AuthRespone(jwt, userDetails.getId());
 
             return ResponseEntity.ok(new ApiRespone("Login successful", authRespone));
+        } catch (AuthenticationException e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                    .body(new ApiRespone("Invalid email or password", null));
+        }
+    }
+    @PostMapping("/register")
+    public ResponseEntity<ApiRespone> registerUser(@RequestBody UserCreateRequest userCreateRequest) {
+        try {
+            userCreateRequest.setRoleIds(java.util.Arrays.asList(2L)); // Default to ROLE_USER
+            User createdUser = userService.createUser(userCreateRequest);
+            
+            
+            Authentication authentication = authManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(userCreateRequest.getEmail(), userCreateRequest.getPassword()));
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            String jwt = jwtUtils.GenerateJwtToken(authentication);
+            BillUserDetail userDetails = (BillUserDetail) authentication.getPrincipal();
+            AuthRespone authRespone = new AuthRespone(jwt, userDetails.getId());
+
+            return ResponseEntity.ok(new ApiRespone("Register successful", authRespone));
         } catch (AuthenticationException e) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
             .body(new ApiRespone("Invalid email or password", null));
